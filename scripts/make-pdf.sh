9eb5e9cd5c164763092543e3eec4c0ce24800a37
Keunwoo Choi <keunwoo.choi@bytedance.com>
2021-11-26 20:06:21 -0500

    fix some; to make pdf look better

--- scripts/make-pdf.sh
+++ scripts/make-pdf.sh
@@ -1,4 +1,8 @@
 # See https://jupyterbook.org/advanced/pdf.html
 
+# [1] pdfhtml
 # pip install pyppeteer
-jupyter-book build book/ --builder pdfhtml
+# jupyter-book build book/ --builder pdfhtml
+
+# [2] with latex
+jupyter-book build book/ --builder pdflatex
